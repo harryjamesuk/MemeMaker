82f78c9bc749f890b294a37ac8ee7f5c186247b6
harryjamesuk <harryjamesuk+github@gmail.com>
2015-02-14 17:58:55 +0000

    Setting up the Database
    Creating an SQLite database that will be used for the meme creation.
    
    Set the DB_NAME and DB_VERSION.
    Set the TABLE.
    Set the COLUMN names.
    Set the SQLite Query to create the table with our columns.
    
    Created constructor that will create/update the database if needed.
    
    Created onCreate() method that will execute our SQL code from
    CREATE_MEMES.
    
    Created onUpgrade() method - This isn't doing anything as of yet.

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeSQLiteHelper.java
@@ -8,10 +8,33 @@ import android.provider.BaseColumns;
 /**
  * Created by Evan Anger on 8/17/14.
  */
-public class MemeSQLiteHelper {
+public class MemeSQLiteHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "memes.db";
+    private static final int DB_VERSION = 1;
     //Meme Table functionality
+    public static final String MEMES_TABLE = "MEMES";
+    public static final String COLUMN_MEME_ASSET = "ASSET";
+    public static final String COLUMN_MEME_NAME = "NAME";
+    private static String CREATE_MEMES =
+    "CREATE TABLE " + MEMES_TABLE + "(" +
+            BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            COLUMN_MEME_ASSET + " TEXT," +
+            COLUMN_MEME_NAME + " TEXT)";
 
     //Meme Table Annotations functionality
 
+    public MemeSQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_MEMES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
